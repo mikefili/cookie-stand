517a613090b7d796a4857ba36e6aa1bdcec35b7c
Mike Filicetti <filicetti.michael@gmail.com>
2018-10-29 22:17:26 -0700

    I was finally able to get the cookiesPerHour to store as a property in each object.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var hoursOfOperation = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
-var soldPerHour = [];
+
 
 
 var storeOne = {
@@ -7,85 +7,93 @@ var storeOne = {
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
-  openHours: [],
+  soldPerHour: [],
   custPerHour: function () {
     for (var i = 0; i < hoursOfOperation.length; i++) {
       var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
       var cookiesPerHour = Math.floor(randCust * this.avgCookies);
-      console.log ([hoursOfOperation[i], cookiesPerHour]);
-      this.openHours.push(cookiesPerHour);
+      // console.log (cookiesPerHour);
+      this.soldPerHour.push(cookiesPerHour);
     }
   }
 };
 
-console.log(storeOne.openHours);
 storeOne.custPerHour();
+console.log(storeOne.soldPerHour);
 
 
-// var storeTwo = {
-//   name: "SeaTac Airport",
-//   minCust: 3,
-//   maxCust: 24,
-//   avgCookies: 1.2,
-//   openHours: [],
-//   custPerHour: function () {
-//     for (var i = 0; i < hoursOfOperation.length; i++) {
-//       var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-//       var cookiesPerHour = Math.floor(randCust * this.avgCookies);
-//       this.soldPerHour.push([hoursOfOperation[i], cookiesPerHour]);
-//     }
-//   }
-// };
+var storeTwo = {
+  name: "SeaTac Airport",
+  minCust: 3,
+  maxCust: 24,
+  avgCookies: 1.2,
+  soldPerHour: [],
+  custPerHour: function () {
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+      var cookiesPerHour = Math.floor(randCust * this.avgCookies);
+      // console.log (cookiesPerHour);
+      this.soldPerHour.push(cookiesPerHour);
+    }
+  }
+};
 
-// storeTwo.custPerHour();
+storeTwo.custPerHour();
+console.log(storeTwo.soldPerHour);
 
-// var storeThree = {
-//   name: "Seattle Center",
-//   minCust: 11,
-//   maxCust: 38,
-//   avgCookies: 3.7,
-//   openHours: [],
-//   custPerHour: function () {
-//     for (var i = 0; i < hoursOfOperation.length; i++) {
-//       var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-//       var cookiesPerHour = Math.floor(randCust * this.avgCookies);
-//       console.log ([hoursOfOperation[i], cookiesPerHour]);
-//     }
-//   }
-// };
+var storeThree = {
+  name: "Seattle Center",
+  minCust: 11,
+  maxCust: 38,
+  avgCookies: 3.7,
+  soldPerHour: [],
+  custPerHour: function () {
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+      var cookiesPerHour = Math.floor(randCust * this.avgCookies);
+      // console.log (cookiesPerHour);
+      this.soldPerHour.push(cookiesPerHour);
+    }
+  }
+};
 
-// storeThree.custPerHour();
+storeThree.custPerHour();
+console.log(storeThree.soldPerHour);
 
-// var storeFour = {
-//   name: "Capitol Hill",
-//   minCust: 20,
-//   maxCust: 38,
-//   avgCookies: 2.3,
-//   openHours: [],
-//   custPerHour: function () {
-//     for (var i = 0; i < hoursOfOperation.length; i++) {
-//       var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-//       var cookiesPerHour = Math.floor(randCust * this.avgCookies);
-//       console.log ([hoursOfOperation[i], cookiesPerHour]);
-//     }
-//   }
-// };
+var storeFour = {
+  name: "Capitol Hill",
+  minCust: 20,
+  maxCust: 38,
+  avgCookies: 2.3,
+  soldPerHour: [],
+  custPerHour: function () {
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+      var cookiesPerHour = Math.floor(randCust * this.avgCookies);
+      // console.log (cookiesPerHour);
+      this.soldPerHour.push(cookiesPerHour);
+    }
+  }
+};
 
-// storeFour.custPerHour();
+storeFour.custPerHour();
+console.log(storeFour.soldPerHour);
 
-// var storeFive = {
-//   name: "Alki",
-//   minCust: 2,
-//   maxCust: 16,
-//   avgCookies: 4.6,
-//   openHours: [],
-//   custPerHour: function () {
-//     for (var i = 0; i < hoursOfOperation.length; i++) {
-//       var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-//       var cookiesPerHour = Math.floor(randCust * this.avgCookies);
-//       console.log ([hoursOfOperation[i], cookiesPerHour]);
-//     }
-//   }
-// };
+var storeFive = {
+  name: "Alki",
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6,
+  soldPerHour: [],
+  custPerHour: function () {
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+      var cookiesPerHour = Math.floor(randCust * this.avgCookies);
+      // console.log (cookiesPerHour);
+      this.soldPerHour.push(cookiesPerHour);
+    }
+  }
+};
 
-// storeFive.custPerHour();
+storeFive.custPerHour();
+console.log(storeFive.soldPerHour);
