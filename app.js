a627a262eee804d656256939c0493c32002783e8
Mike Filicetti <filicetti.michael@gmail.com>
2018-10-29 18:13:37 -0700

    I repeated the process for all five stores.

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ var storeOne = {
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
-  custPerHour: function() {
+  openHours: [],
+  custPerHour: function custPerHour() {
     for (var i = 0; i < hoursOfOperation.length; i++) {
-      var randCust =
-        Math.floor(Math.random() * (this.maxCust - this.minCust)) +
-        this.minCust;
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
       var cookiesPerHour = Math.floor(randCust * this.avgCookies);
       console.log ([hoursOfOperation[i], randCust, cookiesPerHour]);
     }
@@ -17,3 +16,71 @@ var storeOne = {
 };
 
 storeOne.custPerHour();
+
+var storeTwo = {
+  name: "SeaTac Airport",
+  minCust: 3,
+  maxCust: 24,
+  avgCookies: 1.2,
+  openHours: [],
+  custPerHour: function custPerHour() {
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+      var cookiesPerHour = Math.floor(randCust * this.avgCookies);
+      console.log ([hoursOfOperation[i], randCust, cookiesPerHour]);
+    }
+  }
+};
+
+storeTwo.custPerHour();
+
+var storeThree = {
+  name: "Seattle Center",
+  minCust: 11,
+  maxCust: 38,
+  avgCookies: 3.7,
+  openHours: [],
+  custPerHour: function custPerHour() {
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+      var cookiesPerHour = Math.floor(randCust * this.avgCookies);
+      console.log ([hoursOfOperation[i], randCust, cookiesPerHour]);
+    }
+  }
+};
+
+storeThree.custPerHour();
+
+var storeFour = {
+  name: "Capitol Hill",
+  minCust: 20,
+  maxCust: 38,
+  avgCookies: 2.3,
+  openHours: [],
+  custPerHour: function custPerHour() {
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+      var cookiesPerHour = Math.floor(randCust * this.avgCookies);
+      console.log ([hoursOfOperation[i], randCust, cookiesPerHour]);
+    }
+  }
+};
+
+storeFour.custPerHour();
+
+var storeFive = {
+  name: "Alki",
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6,
+  openHours: [],
+  custPerHour: function custPerHour() {
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+      var cookiesPerHour = Math.floor(randCust * this.avgCookies);
+      console.log ([hoursOfOperation[i], randCust, cookiesPerHour]);
+    }
+  }
+};
+
+storeFive.custPerHour();
